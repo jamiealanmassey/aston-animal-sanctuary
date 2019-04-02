e1005b70afac6a924e83d91106a71c31d3841d6e
Jamie Massey <jamiealanmassey@gmail.com>
2019-04-02 02:17:48 +0100

    added more definition to the pending page, included a headless template in the views (for pages that do not require the navbar, and designed the profile page

--- resources/views/pages/pending.blade.php
+++ resources/views/pages/pending.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.default')
 @section('content')
-
 <div class="container">
     <div class="row text-center">
         <div class="col">
             <h1 class="mt-3 inverse-text"><strong>My Pending Adoption Applications</strong></h6>
         </div>
     </div>
-    <div class="row mt-4">
-        @for ($x = 0; $x < 2; $x++)
+    <div class="row mt-4 mb-4">
+        @for ($x = 0; $x < 5; $x++)
             <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
                 <div class="card text-center">
                     <img class="card-img-left" src="img/featured/featured_{{ $x+1 }}.jpg" alt="" width="100%" height="250px">
@@ -17,11 +16,32 @@
                         <p>Some bio about the pet that says how cute they are and how you should adopt them!</p>
                     </div>
                     <div class="card-footer">
-                        <a class="btn btn-danger" href="#" role="button">Withdraw Application</a>
+                        <a class="btn btn-danger" href="#" role="button" data-toggle="modal" data-target="#withdrawModal">Withdraw Application</a>
                     </div>
                 </div>
             </div>
         @endfor
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="withdrawModal" tabindex="-1" role="dialog" aria-labelledby="withdrawModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Warning</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Are you sure you want to withdraw your application to adopt this pet?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Yes</button>
+                <button type="button" class="btn btn-success" data-dismiss="modal">No</button>
+            </div>
+        </div>
     </div>
 </div>
 @stop
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,4 +1,38 @@
-@extends('layouts.default')
+@extends('layouts.headless')
 @section('content')
-<p>Profile Page</p>
+<div class="container inverse-text">
+    <div class="row mt-5">
+        <a href="{{ url('/adopt') }}" class="return ml-3">
+            <h5>Go Back to Aston Animal Santuary</h5>
+        </a>
+    </div>
+    <div class="row mt-5">
+        <div class="col-sm-12 col-md-3">
+            <img src="img/0_200.png" class="rounded" width="250px" height="250px">
+        </div>
+        <div class="col-sm-12 col-md-9">
+            <div class="ml-2">
+                <h3><strong>John Doe</strong></h3>
+                <h6><i class="fas fa-map-marker-alt"></i> Derby, UK</h6>
+                <h6><i class="fas fa-birthday-cake"></i> 03.12.1996</h6>
+                <div class="mt-3">
+                    <h5>Biography</h5>
+                    <p><i>This user has no written biography</i></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-5 text-center">
+        <div class="col">
+            <h2><strong>Past Adoptions</strong></h2>
+            <h4 class="text-muted mt-3">There is nothing to show</h4>
+        </div>
+    </div>
+    <div class="row mt-2 text-center">
+        <div class="col-sm-12 col-md-3">
+        </div>
+        <div class="col-sm-12 col-md-9">
+        </div>
+    </div>
+</div>
 @stop
